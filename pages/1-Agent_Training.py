325afae943d96aa60913e32976778bfc24ab5554
Josh XT <josh@devxt.com>
2023-08-11 15:59:46 -0400

    Add learning from GitHub repos for training

--- pages/1-Agent_Training.py
+++ pages/1-Agent_Training.py
@@ -31,13 +31,14 @@ if agent_name:
                     os.makedirs(os.path.dirname(learn_file_path))
                 with open(learn_file_path, "wb") as f:
                     f.write(learn_file_upload.getbuffer())
-                ApiClient.learn_file(
-                    agent_name=agent_name,
-                    file_name=learn_file_path,
-                    file_content=base64.b64encode(learn_file_upload.read()).decode(
-                        "utf-8"
-                    ),
-                )
+                with st.spinner("Learning, please wait..."):
+                    ApiClient.learn_file(
+                        agent_name=agent_name,
+                        file_name=learn_file_path,
+                        file_content=base64.b64encode(learn_file_upload.read()).decode(
+                            "utf-8"
+                        ),
+                    )
                 st.success(
                     "Agent '"
                     + agent_name
@@ -49,10 +50,38 @@ if agent_name:
         learn_url = st.text_input("Enter a URL for the agent to learn from..")
         if st.button("Learn from URL"):
             if learn_url:
-                learn = ApiClient.learn_url(agent_name=agent_name, url=learn_url)
+                with st.spinner("Learning, please wait..."):
+                    learn = ApiClient.learn_url(agent_name=agent_name, url=learn_url)
                 st.success(
                     f"Agent '{agent_name}' has learned from the URL {learn_url}."
                 )
+
+        st.markdown("### Learn from GitHub Repository")
+        github_repo = st.text_input(
+            "Enter a GitHub repository for the agent to learn from.. For example, 'Josh-XT/AGiXT'"
+        )
+        github_branch = st.text_input(
+            "Enter a GitHub branch. (Default is main)", value="main"
+        )
+        github_user = st.text_input(
+            "Enter the GitHub user or organization name. (Optional, only necessary for private repositories)"
+        )
+        github_token = st.text_input(
+            "Enter a GitHub personal access token. (Optional, only necessary for private repositories)"
+        )
+        if st.button("Learn from GitHub Repository"):
+            if github_repo:
+                with st.spinner("Learning, please wait..."):
+                    learn = ApiClient.learn_github_repo(
+                        agent_name=agent_name,
+                        github_repo=github_repo,
+                        github_user=github_user,
+                        github_token=github_token,
+                    )
+                st.success(
+                    f"Agent '{agent_name}' has learned from the GitHub repository {github_repo}."
+                )
+
         st.markdown("### Wipe Agent Memory")
         st.markdown(
             "The agent can simply learn too much undesired information at times. If you're having an issue with the context being injected from memory with your agent, try wiping the memory."
