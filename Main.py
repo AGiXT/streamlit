f7799f647a456e4e8987fc573e2785ecce7dea47
Josh XT <josh@devxt.com>
2023-08-11 14:41:01 -0400

    Add quick start

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from components.docs import agixt_docs
 
+# Check if session.txt exists
+try:
+    with open("./session.txt") as f:
+        agent_name = f.read()
+except:
+    agent_name = ""
+
 st.set_page_config(
     page_title="AGiXT",
     page_icon=":robot:",
@@ -8,29 +15,45 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-try:
-    with open("./.streamlit/config.toml") as f:
-        if "dark" in f.read():
-            light_theme = False
-except:
-    light_theme = False
-
-
 agixt_docs()
-if light_theme == True:
+
+if agent_name == "":
+    st.markdown("# Getting Started")
     st.markdown(
-        """
-        <div style="text-align: center;">
-        <img src="https://josh-xt.github.io/AGiXT/images/AGiXT-gradient-flat.svg" width="65%">
-        </div>
-        """,
-        unsafe_allow_html=True,
+        "If you do not intend to use the OpenAI agent at this time, go to the [Agent Management](Agent_Management) page and create a new agent for your chosen provider, or modify the default OpenAI agent there."
     )
+    st.markdown("## OpenAI Agent Quick Start")
+    st.markdown(
+        "**If you would like to use an OpenAI agent, please enter your API key below.**"
+    )
+
+    openai_api_key = st.text_input("OpenAI API Key", key="openai_api_key")
+    if st.button("Update API Key"):
+        from ApiClient import ApiClient
+
+        agent_config = ApiClient.get_agentconfig(agent_name="OpenAI")
+        agent_settings = agent_config["settings"]
+        agent_settings["OPENAI_API_KEY"] = openai_api_key
+        ApiClient.update_agent_settings(
+            agent_name="OpenAI",
+            settings=agent_settings,
+        )
+        with open("./session.txt", "w") as f:
+            f.write("OpenAI")
+        st.experimental_rerun()
 else:
+    try:
+        with open("./.streamlit/config.toml") as f:
+            if "dark" in f.read():
+                logo = "AGiXT-gradient-light.svg"
+            else:
+                logo = "AGiXT-gradient-flat.svg"
+    except:
+        logo = "AGiXT-gradient-light.svg"
     st.markdown(
         f"""
         <div style="text-align: center;">
-        <img src="https://josh-xt.github.io/AGiXT/images/AGiXT-gradient-light.svg" width="65%">
+        <img src="https://josh-xt.github.io/AGiXT/images/{logo}" width="65%">
         </div>
         """,
         unsafe_allow_html=True,
