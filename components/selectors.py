a918851320c348c177cb696f053ba52f918bf093
Josh XT <josh@devxt.com>
2023-08-13 17:57:30 -0400

    Move history to selectors

--- components/selectors.py
+++ components/selectors.py
@@ -1,14 +1,9 @@
 from ApiClient import ApiClient
-from components.history import get_history
 import streamlit as st
 import os
 import logging
-
-
-@st.cache_data
-def cached_get_extensions():
-    return ApiClient.get_extensions()
-
+import html
+import re
 
 skip_args = [
     "command_list",
@@ -20,6 +15,88 @@ skip_args = [
     "working_directory",
     "helper_agent_name",
 ]
+
+
+@st.cache_data
+def cached_get_extensions():
+    return ApiClient.get_extensions()
+
+
+def get_history(agent_name, conversation_name):
+    message_container_css = """
+        <style>
+        .message-container {
+            min-height: 200px;
+            height: calc(100vh - 900px);
+            overflow: auto;
+            overflow-y: scroll;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+            padding: 10px;
+            margin: 10px 0;
+        }
+        .message {
+            margin-bottom: 10px;
+        }
+        .user-message {
+            background-color: #0f0f0f;
+            padding: 5px;
+            border-radius: 5px;
+        }
+        .agent-message {
+            background-color: #3a3b3c;
+            color: white;
+            padding: 5px;
+            border-radius: 5px;
+        }
+        </style>
+    """
+
+    st.write(message_container_css, unsafe_allow_html=True)
+
+    with st.container():
+        message_container = "<div class='message-container'>"
+        try:
+            history = ApiClient.get_conversation(
+                agent_name=agent_name,
+                conversation_name=conversation_name,
+                limit=100,
+                page=1,
+            )
+            if isinstance(history, str):
+                st.write(history)
+            else:
+                history.reverse()
+                for item in history:
+                    item["message"] = html.escape(item["message"])
+                    item["message"] = item["message"].replace(r"\n", "<br>")
+                    code_block_match = re.search(
+                        r"```(.*)```", item["message"], re.DOTALL
+                    )
+
+                    if code_block_match:
+                        code_message = code_block_match.group(1)
+                        item["message"] = re.sub(
+                            r"```.*```",
+                            f"<pre><code>{code_message}</code></pre>",
+                            item["message"],
+                            flags=re.DOTALL,
+                        )
+
+                    message = f"{item['timestamp']}<br><b>{item['role']}:</b><br>{item['message']}"
+
+                    if agent_name in item["role"]:
+                        message_container += (
+                            f"<div class='message agent-message'>{message}</div>"
+                        )
+                    else:
+                        message_container += (
+                            f"<div class='message user-message'>{message}</div>"
+                        )
+        except Exception as e:
+            print(e)
+        message_container += "</div>"
+        st.write(message_container, unsafe_allow_html=True)
 
 
 def build_args(args: dict = {}, prompt: dict = {}, step_number: int = 0):
