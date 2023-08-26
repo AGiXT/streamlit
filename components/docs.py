bc59dbd80c8d629c3e744873afed27c7a15ae679
Josh XT <josh@devxt.com>
2023-08-26 09:19:29 -0400

    Add option to hide docs

--- components/docs.py
+++ components/docs.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+HIDE_DOCS = os.getenv("HIDE_DOCS", False)
 
 
 def agixt_docs():
+    if HIDE_DOCS:
+        return
     st.markdown(
         "[![GitHub](https://img.shields.io/badge/GitHub-Sponsor%20Josh%20XT-blue?logo=github&style=plastic)](https://github.com/sponsors/Josh-XT) [![PayPal](https://img.shields.io/badge/PayPal-Sponsor%20Josh%20XT-blue.svg?logo=paypal&style=plastic)](https://paypal.me/joshxt) [![Ko-Fi](https://img.shields.io/badge/Kofi-Sponsor%20Josh%20XT-blue.svg?logo=kofi&style=plastic)](https://ko-fi.com/joshxt)"
     )
@@ -12,6 +19,8 @@ def agixt_docs():
 
 
 def predefined_injection_variables():
+    if HIDE_DOCS:
+        return
     st.markdown(
         """
 ### Predefined Injection Variables
@@ -27,6 +36,8 @@ def predefined_injection_variables():
 
 
 def predefined_memory_collections():
+    if HIDE_DOCS:
+        return
     st.markdown(
         """
 ## Predefined Memory Collections
