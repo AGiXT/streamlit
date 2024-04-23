de1648265d6a82ed0ab286246690e53be1f2eaad
rm4453 <rmattingly4415@gmail.com>
2024-04-23 09:57:40 -0600

    update Main.py

--- Main.py
+++ Main.py
@@ -17,11 +17,12 @@ st.set_page_config(
 )
 
 def check_server_conf():
+    server_response.status_code = 401
     if os.path.isfile("server_conf.json"):
       f = open("server_conf.json")
       data = json.load(f)
       server_response = requests.get(f""+data['SERVER_URI']+"/api/providers", headers={"Authorization": data['API_KEY']})
-    else:
+    elseif {base_uri}:
       server_response = requests.get(f"{base_uri}/api/providers", headers={"Authorization": api_key})
     return server_response.status_code
   
