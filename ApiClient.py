0b0ceeeaa0fa52952641b2b62cf2b597a083d00c
rm4453 <rmattingly4415@gmail.com>
2024-04-23 16:32:32 -0600

    update ApiClient.py

--- ApiClient.py
+++ ApiClient.py
@@ -47,7 +47,7 @@ if serv_resp != 200:
     #st.warning("The API Config Is Invalid - Please Re-Enter Server URL & API Key")
     
     import streamlit as st
-    st.form()
+    st.form():
       s_URI = st.text_input("Server URL:", value = base_uri, key="server_URI")
       s_KEY = st.text_input("Server API Key:", key="server_KEY")
       if st.button("Submit"):
