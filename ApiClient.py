7ac9822048d1788d3ffe9e7be7ce82fa65b6f23b
rm4453 <rmattingly4415@gmail.com>
2024-04-23 13:59:00 -0600

    update ApiClient.py

--- ApiClient.py
+++ ApiClient.py
@@ -54,7 +54,7 @@ if serv_resp != 200:
         output_json = {"SERVER_URI" : s_URI,"API_KEY" : s_KEY}
         with open("server_conf.json", "w+") as outfile:
             json.dump(output_json, outfile)
-            close(outfile)
+            outfile.close()
         st.rerun()
     st.stop()
     
