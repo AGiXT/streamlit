424ba7a89063acdbbffcd36e80605fec675c796a
rm4453 <rmattingly4415@gmail.com>
2024-04-23 14:03:51 -0600

    update ApiClient.py

--- ApiClient.py
+++ ApiClient.py
@@ -52,7 +52,7 @@ if serv_resp != 200:
     s_KEY = st.text_input("Server API Key:", key="server_KEY")
     if st.button("Submit"):
         output_json = {"SERVER_URI" : s_URI,"API_KEY" : s_KEY}
-        with open("server_conf.json", "w+") as outfile:
+        with open("server_conf.json", "w") as outfile:
             f.write(json.dumps(output_json))
         st.rerun()
     st.stop()
