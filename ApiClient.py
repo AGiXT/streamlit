8b6abcdc493b3fd30c3d5db70aa15343a3e8af1c
rm4453 <rmattingly4415@gmail.com>
2024-04-23 16:31:35 -0600

    update ApiClient.py

--- ApiClient.py
+++ ApiClient.py
@@ -47,15 +47,15 @@ if serv_resp != 200:
     #st.warning("The API Config Is Invalid - Please Re-Enter Server URL & API Key")
     
     import streamlit as st
-
-    s_URI = st.text_input("Server URL:", value = base_uri, key="server_URI")
-    s_KEY = st.text_input("Server API Key:", key="server_KEY")
-    if st.button("Submit"):
-        output_json = {"SERVER_URI": s_URI,"API_KEY": s_KEY}
-        while os.path.isfile("./server_conf.json") == False:
-            with open("./server_conf.json", "w") as outfile:
-                json.dump(output_json, outfile)
-        st.rerun()
+    st.form()
+      s_URI = st.text_input("Server URL:", value = base_uri, key="server_URI")
+      s_KEY = st.text_input("Server API Key:", key="server_KEY")
+      if st.button("Submit"):
+          output_json = {"SERVER_URI": s_URI,"API_KEY": s_KEY}
+          while os.path.isfile("./server_conf.json") == False:
+              with open("./server_conf.json", "w") as outfile:
+                  json.dump(output_json, outfile)
+          st.rerun()
     st.stop()
     
 load_dotenv()
